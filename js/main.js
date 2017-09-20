c25bcafb487c14f03b4b472726b693ade8b58790
Piffek <patrykpiwko123412@gmail.com>
2017-09-20 20:56:42 +0200

    js listener

--- js/main.js
+++ js/main.js
@@ -15,25 +15,48 @@ document.addEventListener('DOMContentLoaded', function () {
         'gerant.png'
     ];
     const c = [];
-    for (var i = 0; i < 11; ++i) {
-        c[i] = document.getElementById('card'+i);
+    for (var i = 0; i <= 11; ++i) {
+        c[i] = document.getElementById('card' + i);
     }
+    c[0].addEventListener('click', function () {
+        reverseCard(c[0])
+    });
+    c[1].addEventListener('click', function () {
+        reverseCard(c[1])
+    });
+    c[2].addEventListener('click', function () {
+        reverseCard(c[2])
+    });
     c[3].addEventListener('click', function () {
-        alert(c[3]);
+        reverseCard(c[3])
+    });
+    c[4].addEventListener('click', function () {
+        reverseCard(c[4])
+    });
+    c[5].addEventListener('click', function () {
+        reverseCard(c[5])
+    });
+    c[6].addEventListener('click', function () {
+        reverseCard(c[6])
+    });
+    c[7].addEventListener('click', function () {
+        reverseCard(c[7])
+    });
+    c[8].addEventListener('click', function () {
+        reverseCard(c[8])
+    });
+    c[9].addEventListener('click', function () {
+        reverseCard(c[9])
+    });
+    c[10].addEventListener('click', function () {
+        reverseCard(c[10])
+    });
+    c[11].addEventListener('click', function () {
+        reverseCard(c[11])
     });
 
-    /*
-    const c0 = document.getElementById('c0');
-    const c1 = document.getElementById('c1');
-    const c2 = document.getElementById('c2');
-    const c3 = document.getElementById('c3');
-    const c4 = document.getElementById('c4');
-    const c5 = document.getElementById('c5');
-    const c6 = document.getElementById('c6');
-    const c7 = document.getElementById('c7');
-    const c8 = document.getElementById('c8');
-    const c9 = document.getElementById('c9');
-    const c10 = document.getElementById('c10');
-    const c11 = document.getElementById('c11');
-    */
+    function reverseCard(cardID) {
+        console.log(cardID.id);
+    }
+
 });
