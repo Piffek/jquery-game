61c23298dea2081d6fee247cc2ad432c0c010f8b
Piffek <patrykpiwko123412@gmail.com>
2017-10-05 21:49:53 +0200

    generate random elements in array

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 document.addEventListener('DOMContentLoaded', function () {
-    const cards = [
+	const cards = [
         'ciri.png',
         'iorweth.png',
         'ciri.png',
@@ -14,101 +14,116 @@ document.addEventListener('DOMContentLoaded', function () {
         'yen.png',
         'geralt.png'
     ];
-    const folderWIthImage = '/img/';
-    const c = [];
-    const score = document.getElementById('score');
-    let firstCard = false;
-    let scoreExtend = 0;
-    let firstCard_nr = 0;
-    let pairs = 6;
-    let lock = false;
 
 
-    for (var i = 0; i <= 11; ++i) {
-        c[i] = document.getElementById('card' + i);
-    }
-    c[0].addEventListener('click', function () {
-        reverseCard(0, c[0])
-    });
-    c[1].addEventListener('click', function () {
-        reverseCard(1, c[1])
-    });
-    c[2].addEventListener('click', function () {
-        reverseCard(2, c[2])
-    });
-    c[3].addEventListener('click', function () {
-        reverseCard(3, c[3])
-    });
-    c[4].addEventListener('click', function () {
-        reverseCard(4, c[4])
-    });
-    c[5].addEventListener('click', function () {
-        reverseCard(5, c[5])
-    });
-    c[6].addEventListener('click', function () {
-        reverseCard(6, c[6])
-    });
-    c[7].addEventListener('click', function () {
-        reverseCard(7, c[7])
-    });
-    c[8].addEventListener('click', function () {
-        reverseCard(8, c[8])
-    });
-    c[9].addEventListener('click', function () {
-        reverseCard(9, c[9])
-    });
-    c[10].addEventListener('click', function () {
-        reverseCard(10, c[10])
-    });
-    c[11].addEventListener('click', function () {
-        reverseCard(11, c[11])
-    });
+	var array = [];
 
-    function reverseCard(nr, cardID) {
-        const image = folderWIthImage + cards[nr];
-        let opacityThisElement = cardID.style.opacity;
 
-        if (opacityThisElement == '' && lock == false) {
-            lock = true;
-            cardID.style.backgroundImage = 'url(' + image + ')';
-            cardID.style.border = '3px solid #e9b64a';
-            if (firstCard && firstCard_nr !== nr) {
 
-                if (cards[firstCard_nr] === cards[nr]) {
-                    setTimeout(function () {
-                        c[nr].style.opacity = 0;
-                        c[firstCard_nr].style.opacity = 0;
-                        lock = false;
+	for (var i = 0; i <= 11; ++i) {
+		array.push(cards[Math.floor(Math.random() * cards.length)]);
 
-                        pairs--;
-                        if (pairs === 0) {
-                            document.getElementById('board').innerHTML = 'You win in round' + scoreExtend;
-                        }
-                    }, 750);
+	};
 
-                } else {
 
-                    setTimeout(function () {
-                        c[nr].style.backgroundImage = 'url(/img/karta.png)';
-                        c[firstCard_nr].style.backgroundImage = 'url(/img/karta.png)';
-                        c[nr].style.border = '3px solid blue';
-                        c[firstCard_nr].style.border = '3px solid blue';
-                        lock = false;
-                    }, 1000);
 
-                }
+	const folderWIthImage = '/img/';
+	const c = [];
+	const score = document.getElementById('score');
+	let firstCard = false;
+	let scoreExtend = 0;
+	let firstCard_nr = 0;
+	let pairs = 6;
+	let lock = false;
 
-                score.innerHTML = 'Score: ' + ++scoreExtend;
-                firstCard = false;
 
-            } else {
+	for (var i = 0; i <= 11; ++i) {
+		c[i] = document.getElementById('card' + i);
+	}
 
-                firstCard = true;
-                firstCard_nr = nr;
-                lock = false;
-            }
-        }
 
-    }
+	c[0].addEventListener('click', function () {
+		reverseCard(0, c[0])
+	});
+	c[1].addEventListener('click', function () {
+		reverseCard(1, c[1])
+	});
+	c[2].addEventListener('click', function () {
+		reverseCard(2, c[2])
+	});
+	c[3].addEventListener('click', function () {
+		reverseCard(3, c[3])
+	});
+	c[4].addEventListener('click', function () {
+		reverseCard(4, c[4])
+	});
+	c[5].addEventListener('click', function () {
+		reverseCard(5, c[5])
+	});
+	c[6].addEventListener('click', function () {
+		reverseCard(6, c[6])
+	});
+	c[7].addEventListener('click', function () {
+		reverseCard(7, c[7])
+	});
+	c[8].addEventListener('click', function () {
+		reverseCard(8, c[8])
+	});
+	c[9].addEventListener('click', function () {
+		reverseCard(9, c[9])
+	});
+	c[10].addEventListener('click', function () {
+		reverseCard(10, c[10])
+	});
+	c[11].addEventListener('click', function () {
+		reverseCard(11, c[11])
+	});
+
+	function reverseCard(nr, cardID) {
+		const image = folderWIthImage + array[nr];
+		let opacityThisElement = cardID.style.opacity;
+
+		if (opacityThisElement == '' && lock == false) {
+			lock = true;
+			cardID.style.backgroundImage = 'url(' + image + ')';
+			cardID.style.border = '3px solid #e9b64a';
+			if (firstCard && firstCard_nr !== nr) {
+
+				if (array[firstCard_nr] === array[nr]) {
+					setTimeout(function () {
+						c[nr].style.opacity = 0;
+						c[firstCard_nr].style.opacity = 0;
+						lock = false;
+
+						pairs--;
+						if (pairs === 0) {
+							document.getElementById('board').innerHTML = 'You win in round' + scoreExtend;
+						}
+					}, 750);
+
+				} else {
+
+					setTimeout(function () {
+						c[nr].style.backgroundImage = 'url(/img/karta.png)';
+						c[firstCard_nr].style.backgroundImage = 'url(/img/karta.png)';
+						c[nr].style.border = '3px solid blue';
+						c[firstCard_nr].style.border = '3px solid blue';
+						lock = false;
+					}, 1000);
+
+				}
+
+				score.innerHTML = 'Score: ' + ++scoreExtend;
+				firstCard = false;
+
+			} else {
+
+				firstCard = true;
+				firstCard_nr = nr;
+				lock = false;
+			}
+		}
+
+	}
 
 });
